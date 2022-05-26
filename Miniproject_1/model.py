22079a98dc602c46354784bcaaec8d7deca115e8
TemryL <tommery01@gmail.com>
2022-05-26 18:09:46 +0200

    final model

--- Miniproject_1/model.py
+++ Miniproject_1/model.py
@@ -9,23 +9,24 @@ class Model(nn.Module):
         super().__init__()
         
         #Encoder
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3)  
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=3)
+        self.conv1 = nn.Conv2d(3, 100, kernel_size=3)  
+        self.conv2 = nn.Conv2d(100, 100, kernel_size=3)
         
         #Decoder
-        self.t_conv1 = nn.ConvTranspose2d(32, 32, kernel_size=3)
-        self.t_conv2 = nn.ConvTranspose2d(32, 3, kernel_size=3)
+        self.t_conv1 = nn.ConvTranspose2d(100, 100, kernel_size=3)
+        self.t_conv2 = nn.ConvTranspose2d(2*100, 3, kernel_size=3)
         
         self.optimizer = optim.Adam(self.parameters(), lr = 1e-3)
         self.criterion = nn.MSELoss()
         
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.to(self.device)  
-    
+        
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
+        res = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(res))
         x = F.relu(self.t_conv1(x))
+        x = torch.cat((res, x), dim=1) 
         x = torch.sigmoid(self.t_conv2(x))
         return x
     
@@ -36,26 +37,25 @@ class Model(nn.Module):
         self.load_state_dict(m_state_dict)
     
     def train(self, train_input, train_target, num_epochs):
-        # train_input : tensor of size (N, C, H, W) containing a noisy version of the images, with values in range 0-255.
-        # train_target : tensor of size (N, C, H, W) containing another noisy version of the 
-        # same images , which only differs from the input by their noise, with values in range 0-255.
-        
-        # Normalize data
-        train_input = train_input.div(255.0)
-        train_target = train_target.div(255.0)
-        
-        mini_batch_size = 1000
-        for e in range(num_epochs):
-            epoch_loss = 0
-            for b in range(0, train_input.size(0), mini_batch_size):
-                print(b)
-                output = self(train_input.narrow(0, b, mini_batch_size))
-                loss = self.criterion(output, train_target.narrow(0, b, mini_batch_size))
-                epoch_loss += loss.item()
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-            print("Epoch {}: Loss {}".format(e, epoch_loss))
+            # train_input : tensor of size (N, C, H, W) containing a noisy version of the images, with values in range 0-255.
+            # train_target : tensor of size (N, C, H, W) containing another noisy version of the 
+            # same images , which only differs from the input by their noise, with values in range 0-255.
+            
+            # Normalize data
+            train_input = train_input.div(255.0)
+            train_target = train_target.div(255.0)
+            
+            mini_batch_size = 8
+            for e in range(num_epochs):
+                epoch_loss = 0
+                for b in range(0, train_input.size(0), mini_batch_size):
+                    output = self(train_input.narrow(0, b, mini_batch_size))
+                    loss = self.criterion(output, train_target.narrow(0, b, mini_batch_size))
+                    epoch_loss += loss.item()
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
+                print("Epoch {}: Loss {}".format(e, epoch_loss))
     
     def predict(self, test_input):
         # test_input : tensor of size (N1 , C, H, W) with values in range 0-255 that has to be denoised by the trained
